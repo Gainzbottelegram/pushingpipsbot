c4fbd5ceb030d541b90293bd1241096150ff35d9
Gainzbottelegram <connorlogan98@yahoo.com>
2025-04-19 01:21:23 +0100

    language update final 2.0

--- bot.py
+++ bot.py
@@ -515,7 +515,6 @@ if __name__ == "__main__":
     print("🤖 GainzBot is live and flexing!")
     app.run_polling()
 
-=======
 
 from kraken_client import get_price  # Make sure this is your custom function
 
